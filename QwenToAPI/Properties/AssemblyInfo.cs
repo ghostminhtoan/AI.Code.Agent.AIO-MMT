520d0785387f00469bac0ab455ad69741d029fd9
ghostminhtoan <ghostminhtoan@gmail.com>
2026-03-19 09:09:31 +0700

    Rename project to AI Code Agent AIO - MMT

--- QwenToAPI/Properties/AssemblyInfo.cs
+++ QwenToAPI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QwenToAPI")]
+[assembly: AssemblyTitle("AI Code Agent AIO - MMT")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QwenToAPI")]
+[assembly: AssemblyProduct("AI Code Agent AIO - MMT")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
